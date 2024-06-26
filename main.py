df4ad977fb3d3c1029fe674ab28ec18b173b66ba
he11ah0und <60035122+he11ah0und@users.noreply.github.com>
2024-06-26 18:56:45 +1000

    Update main.py
    Единичное использование print()

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def check_duplicates(input_string):
     elements = input_string.split()
     unique_elements = set(elements)
+    text = "Повторяющихся значений нет"
     if len(elements) != len(unique_elements):
-        print("Есть повторяющиеся значения")
-    else:
-        print("Повторяющихся значений нет")
+        text = "Есть повторяющиеся значения"
+    print(text)
 
 # Запрос строки у пользователя
 user_input = input("Введите строку: ")
-check_duplicates(user_input)
+check_duplicates(user_input) # Выполняем проверку
